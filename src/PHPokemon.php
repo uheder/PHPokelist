4f6cc60fd181112b32b7ccab3bf5d547ccbef5c9
Uheder Macedo <dev.usmacedo@outlook.com>
2024-09-16 07:22:21 -0300

    feat: add project and tests

---
+++ src/PHPokemon.php
@@ -0,0 +1,32 @@
+<?php
+
+class PHPokemon
+{
+    public string $name;
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getUrl(): string
+    {
+        return "https://pokeapi.co/api/v2/pokemon/" . $this->name;
+    }
+
+    public function getPokeData() {
+        $data = curl_init($this->getUrl());
+        if (curl_errno($data)){
+            return false;
+        } else {
+            curl_setopt($data, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($data, CURLOPT_SSL_VERIFYPEER, false);
+            return json_decode(curl_exec($data));
+        }
+    }
+
+    public function getSprite() {
+        $data = $this->getPokeData();
+        return $data->sprites->front_default;
+    }
+}
---
+++ tests/PHPokemonTest.php
@@ -0,0 +1,33 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once './src/PHPokemon.php';
+
+class PHPokemonTest extends TestCase {
+    protected PHPokemon $pikachu;
+    protected PHPokemon $bulbasaur;
+
+    protected function setUp(): void
+    {
+        $this->pikachu = new PHPokemon('pikachu');
+        $this->bulbasaur = new PHPokemon('bulbasaur');
+    }
+
+    public function testGetUrl(): void {
+        $this->assertEquals('https://pokeapi.co/api/v2/pokemon/pikachu', $this->pikachu->getUrl());
+        $this->assertEquals('https://pokeapi.co/api/v2/pokemon/bulbasaur', $this->bulbasaur->getUrl());
+    }
+
+    public function testGetPokeData(): void {
+        $this->assertEquals('pikachu', $this->pikachu->getPokeData()->name);
+        $this->assertEquals(25, $this->pikachu->getPokeData()->id);
+
+        $this->assertEquals('bulbasaur', $this->bulbasaur->getPokeData()->name);
+        $this->assertEquals(1, $this->bulbasaur->getPokeData()->id);
+    }
+
+    public function testGetSprite(): void {
+        $this->assertEquals('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png', $this->pikachu->getSprite());
+    }
+}
